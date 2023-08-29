dbbdcbcf7b0d1159582b25abe328f211b90e72f5
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2023-08-29 14:04:31 -0300

    Add files via upload

--- assets/components/contact/contact.js
+++ assets/components/contact/contact.js
@@ -2,6 +2,26 @@ const iname=document.getElementById('iname')
 const imail=document.getElementById('imail')
 const iwhats=document.getElementById('iwhats')
 const imsg=document.getElementById('imsg')
+const submitbtn=document.getElementById('submitbtn')
+var returnSend=document.getElementById('returnSend')
 
 
+getAlertSend=()=>{
+    returnSend.classList.toggle('show')
+    submitbtn.style.cssText='opacity:0.3'
+    submitbtn.setAttribute('disable',true)
+}
 
+
+const btnOK=document.querySelector('.footer-controls button')
+
+
+btnOK.addEventListener('click',()=>{
+    var returnSend=document.getElementById('returnSend')
+    returnSend.classList.toggle('show')
+
+     
+    setTimeout(() => {
+        location.reload()
+      }, 1000);
+})
