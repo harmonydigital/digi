e2ed50c1341f7caba29ca17555b2d0bf2f4f4b76
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2023-09-03 15:49:47 -0300

    v0.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,8 +21,18 @@ initapp=()=>{
         <header>
             <div class='brand'><a href='index.html'><img class='logo' src='assets/img/beija.png'></a></div>
             <div class='menu'>
-
+            <button onclick='menutoggleMenu()' class='btn-menu'>
+            <i class="fa-solid fa-bars"></i>
+            </button>
                 <nav>
+                    <div class='controls' onclick='closeMenu()' >
+                        <button>
+                        <svg class="svg-inline--fa fa-chevron-left" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg=""><path fill="currentColor" d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"></path></svg>
+                        </button>
+                        <button onclick='closeMenu()' >
+                        <svg class="svg-inline--fa fa-xmark" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"></path></svg>
+                        </button>
+                    </div>
                     <ul>
                         <li>
                             <a href='#portfolio'>Galeria</a>
@@ -83,7 +93,7 @@ initapp=()=>{
                         <div class='content-text'>
                         <span class='title'></span>
                         <h2>produtos
-                        para pessoas
+                        para pessoas,
                         como eu e você.</h2>
                         <a class='anchor btn primary convert-btn h-xs' href='#about'>
                         <span>Fale comigo</span>
@@ -93,8 +103,8 @@ initapp=()=>{
 
                         <div>
                             <p>Harmony se traduz na eficiente interação entre produtos e pessoas.
-                            </br></br>Somos a estreita relação do homem e tecnologia.
-                            Amamos criar soluções reais e tornar a vida de pessoas ainda melhor através de soluções tecnologicas.
+                            </br></br>Somos a estreita relação do homem e tecnologia, amamos criar soluções reais para tornar a vida de pessoas ainda
+                            melhor.
                             </br></br>Unimos um grupo de profissionais do Design, para conversas sobre o comportamento humano e como a interação de alguns produtos podem ser tão relevantes, assim projetar novos modelos e testar novas soluções.
                             </br></br>Aqui tudo começa na relação de pessoa para pessoa, criamos uma conexão com o interlocutor e então podemos entender como o Design pode solucionar e otimizar nossa vida enquanto seres humanos.
                             Veja o que fizemos até aqui, e crie algo novo você também..</p>
